a1df349ec2578f7baa0aa6caba4ac8ba30dd1cf0
Jacob Rief <jacob.rief@gmail.com>
2013-10-13 14:57:30 +0200

    Initial revision

---
+++ angular-placeholder.js
@@ -0,0 +1,79 @@
+/*
+ * angular-shims-placeholder
+ * https://github.com/jrief/angular-shims-placeholder
+ *
+ * Add Angular directives which emulates attribute ´placeholder´ in input fields
+ * of type text for browsers not supporting this, ie. IE8 and below.
+ *
+ * Copyright (c) 2013 Jacob Rief
+ * Licensed under the MIT license.
+ */
+
+(function(angular, undefined) {
+'use strict';
+
+angular.module('ng.shims.placeholder', []).directive('placeholder', function() {
+	var test = document.createElement("input");
+	if (test.placeholder !== void 0)
+		return {};
+
+	// No native support for attribute placeholder
+	return {
+		restrict: 'A',
+		require: 'ngModel',
+		link: function(scope, elem, attrs, ngModel) {
+			var orig_type = attrs.type;
+
+			// load initial value from element
+			if (!elem.val()) {
+				elem.addClass('empty');
+				elem.val(attrs.placeholder);
+				if (orig_type === 'password') {
+					elem.attr('type', 'text');
+				}
+				ngModel.$setViewValue('');
+			} else {
+				ngModel.$setViewValue(elem.val());
+			}
+
+			// on focus, replace auto-label with empty field
+			elem.bind('focus', function() {
+				if (elem.hasClass('empty')) {
+					elem.val('');
+					elem.removeClass('empty error');
+					elem.attr('type', orig_type);
+				}
+			});
+
+			// view -> model
+			elem.bind('blur', function() {
+				var orig_val = elem.val();
+				setValue(orig_val);
+				scope.$apply(function() {
+					ngModel.$setViewValue(orig_val);
+				});
+			});
+
+			// model -> view
+			ngModel.$render = function() {
+				setValue(ngModel.$viewValue);
+			};
+
+			function setValue(val) {
+				if (!val) {
+					elem.addClass('empty');
+					elem.val(attrs.placeholder);
+					if (orig_type === 'password') {
+						elem.attr('type', 'text');
+					}
+				} else {
+					elem.removeClass('empty');
+					elem.val(val);
+				}
+			}
+
+		}
+	};
+});
+
+})(window.angular);
---
+++ unit/angular-placeholder-spec.js
@@ -0,0 +1,125 @@
+'use strict';
+
+
+describe('test module angular-placeholder', function() {
+	beforeEach(function() {
+		module('ng.shims.placeholder');
+	});
+
+	describe('for an empty input field of type text using directive placeholder', function() {
+		var email_field, scope;
+
+		beforeEach(inject(function($rootScope, $compile) {
+			scope = $rootScope.$new();
+			email_field = angular.element('<input type="text" name="useremail" placeholder="E-Mail" ng-model="form.email" value="" />');
+			$compile(email_field)(scope);
+		}));
+
+		it('should display the placeholder as input value', function() {
+			expect(email_field.val()).toBe('E-Mail');
+			expect(email_field.hasClass('empty')).toBe(true);
+			expect(scope.form.email).toBe('');
+		});
+
+		describe('when input field gains focus', function() {
+			beforeEach(function() {
+				email_field.triggerHandler('focus');
+			});
+
+			afterEach(function() {
+				email_field.triggerHandler('blur');
+			});
+
+			it('should hide the placeholder and remove class "empty"', function() {
+				expect(email_field.val()).toBe('');
+				expect(email_field.hasClass('empty')).toBe(false);
+				expect(scope.form.email).toBe('');
+			});
+
+			it('should restore the placeholder and class "empty" when leaving field unchanged', function() {
+				email_field.triggerHandler('blur');
+				expect(email_field.val()).toBe('E-Mail');
+				expect(email_field.hasClass('empty')).toBe(true);
+				expect(scope.form.email).toBe('');
+			});
+
+			describe('when text is entered into the input field', function() {
+				beforeEach(function() {
+					email_field.val('me@example.com');
+					email_field.triggerHandler('blur');
+				});
+
+				it('should remember the entered text in scope.form and remove class "empty"', function() {
+					expect(email_field.val()).toBe('me@example.com');
+					expect(email_field.hasClass('empty')).toBe(false);
+					expect(scope.form.email).toBe('me@example.com');
+				});
+
+				it('should reset the field as "empty"', function() {
+					email_field.triggerHandler('focus');
+					email_field.val('');
+					email_field.triggerHandler('blur');
+					expect(email_field.val()).toBe('E-Mail');
+					expect(email_field.hasClass('empty')).toBe(true);
+					expect(scope.form.email).toBe('');
+				});
+			});
+		});
+
+		describe('when the value is set through the model controller', function() {
+			beforeEach(function() {
+				expect(email_field.val()).toBe('E-Mail');
+				scope.form.email = 'john@doe.com';
+				scope.$digest();
+			});
+
+			it('should display the value in the input field', function() {
+				expect(email_field.val()).toBe('john@doe.com');
+				expect(email_field.hasClass('empty')).toBe(false);
+			});
+
+			it('should replace an empty value in the input field by placeholder', function() {
+				scope.form.email = '';
+				scope.$digest();
+				expect(email_field.val()).toBe('E-Mail');
+				expect(email_field.hasClass('empty')).toBe(true);
+			});
+
+		});
+
+	});
+
+	describe('for input field of type password using directive placeholder', function() {
+		var pwd_field, scope;
+
+		beforeEach(inject(function($rootScope, $compile) {
+			scope = $rootScope.$new();
+			pwd_field = angular.element('<input type="password" name="userpwd" placeholder="Password" ng-model="form.passwd" value="" />');
+			$compile(pwd_field)(scope);
+		}));
+
+		it('should display the placeholder as input value for a text field', function() {
+			expect(pwd_field.val()).toBe('Password');
+			expect(pwd_field.attr('type')).toBe('text');
+			expect(pwd_field.hasClass('empty')).toBe(true);
+			expect(scope.form.passwd).toBe('');
+		});
+
+		describe('when password field gains focus', function() {
+			beforeEach(function() {
+				pwd_field.triggerHandler('focus');
+			});
+
+			afterEach(function() {
+				pwd_field.triggerHandler('blur');
+			});
+
+			it('should hide the placeholder, remove class "empty" and obfuscate text input', function() {
+				expect(pwd_field.val()).toBe('');
+				expect(pwd_field.hasClass('empty')).toBe(false);
+				expect(scope.form.passwd).toBe('');
+				expect(pwd_field.attr('type')).toBe('password');
+			});
+		});
+	});
+});
