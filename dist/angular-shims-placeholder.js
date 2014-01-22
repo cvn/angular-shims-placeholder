c46e13726dd8a0b7b3a55ade822b662207479e78
Nicolas <rudas.n@gmail.com>
2014-01-22 13:25:57 +0200

    Support for password inputs, without changing the type property

--- dist/angular-shims-placeholder.js
+++ dist/angular-shims-placeholder.js
@@ -1,9 +1,9 @@
 /*! angular-shims-placeholder - v0.1.0 - 2013-10-14
 * https://github.com/jrief/angular-shims-placeholder
 * Copyright (c) 2013 Jacob Rief; Licensed MIT */
-(function (angular, undefined) {
+(function(angular, document, undefined) {
   'use strict';
-  angular.module('ng.shims.placeholder', []).directive('placeholder', function () {
+  angular.module('ng.shims.placeholder', []).directive('placeholder', function() {
     if (!angular.mock) {
       var test = document.createElement('input');
       if (test.placeholder !== void 0)
@@ -12,48 +12,65 @@
     return {
       restrict: 'A',
       require: 'ngModel',
-      link: function (scope, elem, attrs, ngModel) {
-        var orig_type = attrs.type;
-        if (!elem.val()) {
-          elem.addClass('empty');
-          elem.val(attrs.placeholder);
-          if (orig_type === 'password') {
-            elem.attr('type', 'text');
-          }
-          ngModel.$setViewValue('');
-        } else {
-          ngModel.$setViewValue(elem.val());
+      link: function(scope, elem, attrs, ngModel) {
+        var orig_type = attrs.type,
+            is_pwd = orig_type === 'password',
+            text = attrs.placeholder,
+            emptyClassName = 'empty',
+            clone;
+        if (!text) { return; }
+        if (is_pwd) {
+            clone = elem.clone().attr({
+                    'type': 'text',
+                    'value': text,
+                    'placeholder': ''
+                })
+                .hide()
+                .bind('focus', function(e) {
+                    clone.hide();
+                    elem.show().focus();
+                })
+                .insertBefore(elem);
+            elem.bind('blur', function(e) {
+                    if (!elem.val()) {
+                        elem.hide();
+                        clone.show();
+                    }
+                });
         }
-        elem.bind('focus', function () {
-          if (elem.hasClass('empty')) {
+        ngModel.$setViewValue(elem.val());
+        setValue(elem.val());
+        elem.bind('focus', function() {
+          if (elem.hasClass(emptyClassName)) {
             elem.val('');
-            elem.removeClass('empty error');
-            elem.attr('type', orig_type);
+            elem.removeClass(emptyClassName);
           }
         });
-        elem.bind('blur', function () {
+        elem.bind('blur', function() {
           var orig_val = elem.val();
-          setValue(orig_val);
-          scope.$apply(function () {
+          scope.$apply(function() {
+            setValue(orig_val);
             ngModel.$setViewValue(orig_val);
           });
         });
-        ngModel.$render = function () {
+        ngModel.$render = function() {
           setValue(ngModel.$viewValue);
         };
         function setValue(val) {
           if (!val) {
-            elem.addClass('empty');
-            elem.val(attrs.placeholder);
-            if (orig_type === 'password') {
-              elem.attr('type', 'text');
+            elem.addClass(emptyClassName);
+            elem.val(text);
+            if (is_pwd) {
+                elem.hide();
+                clone.show();
+                clone.addClass(emptyClassName);
             }
           } else {
-            elem.removeClass('empty');
+            elem.removeClass(emptyClassName);
             elem.val(val);
           }
         }
       }
     };
   });
-}(window.angular));
+}(window.angular, window.document));
