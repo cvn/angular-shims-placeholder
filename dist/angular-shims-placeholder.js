f05c0d8b805a0927a861e27777a1f628b41cff79
Jacob Rief <jacob.rief@gmail.com>
2014-03-21 09:01:57 +0100

    Added minified files

--- dist/angular-shims-placeholder.js
+++ dist/angular-shims-placeholder.js
@@ -1,9 +1,9 @@
-/*! angular-shims-placeholder - v0.1.0 - 2013-10-14
+/*! angular-shims-placeholder - v0.1.0 - 2014-03-21
 * https://github.com/jrief/angular-shims-placeholder
-* Copyright (c) 2013 Jacob Rief; Licensed MIT */
-(function(angular, document, undefined) {
+* Copyright (c) 2014 Jacob Rief; Licensed MIT */
+(function (angular, undefined) {
   'use strict';
-  angular.module('ng.shims.placeholder', []).directive('placeholder', function() {
+  angular.module('ng.shims.placeholder', []).directive('placeholder', function () {
     if (!angular.mock) {
       var test = document.createElement('input');
       if (test.placeholder !== void 0)
@@ -12,65 +12,49 @@
     return {
       restrict: 'A',
       require: 'ngModel',
-      link: function(scope, elem, attrs, ngModel) {
-        var orig_type = attrs.type,
-            is_pwd = orig_type === 'password',
-            text = attrs.placeholder,
-            emptyClassName = 'empty',
-            clone;
-        if (!text) { return; }
-        if (is_pwd) {
-            clone = elem.clone().attr({
-                    'type': 'text',
-                    'value': text,
-                    'placeholder': ''
-                })
-                .hide()
-                .bind('focus', function(e) {
-                    clone.hide();
-                    elem.show().focus();
-                })
-                .insertBefore(elem);
-            elem.bind('blur', function(e) {
-                    if (!elem.val()) {
-                        elem.hide();
-                        clone.show();
-                    }
-                });
+      priority: 1,
+      link: function (scope, elem, attrs, ngModel) {
+        var orig_type = attrs.type;
+        if (!elem.val()) {
+          elem.addClass('empty');
+          elem.val(attrs.placeholder);
+          if (orig_type === 'password') {
+            elem.attr('type', 'text');
+          }
+          ngModel.$setViewValue('');
+        } else {
+          ngModel.$setViewValue(elem.val());
         }
-        ngModel.$setViewValue(elem.val());
-        setValue(elem.val());
-        elem.bind('focus', function() {
-          if (elem.hasClass(emptyClassName)) {
+        elem.bind('focus', function () {
+          if (elem.hasClass('empty')) {
             elem.val('');
-            elem.removeClass(emptyClassName);
+            elem.removeClass('empty error');
+            elem.attr('type', orig_type);
           }
         });
-        elem.bind('blur', function() {
+        elem.bind('blur', function () {
           var orig_val = elem.val();
-          scope.$apply(function() {
-            setValue(orig_val);
+          setValue(orig_val);
+          scope.$apply(function () {
             ngModel.$setViewValue(orig_val);
           });
         });
-        ngModel.$render = function() {
+        ngModel.$render = function () {
           setValue(ngModel.$viewValue);
         };
         function setValue(val) {
           if (!val) {
-            elem.addClass(emptyClassName);
-            elem.val(text);
-            if (is_pwd) {
-                elem.hide();
-                clone.show();
-                clone.addClass(emptyClassName);
+            elem.addClass('empty');
+            elem.val(attrs.placeholder);
+            if (orig_type === 'password') {
+              elem.attr('type', 'text');
             }
           } else {
-            elem.removeClass(emptyClassName);
+            elem.removeClass('empty');
             elem.val(val);
           }
         }
       }
     };
   });
-}(window.angular, window.document));
+}(window.angular));
