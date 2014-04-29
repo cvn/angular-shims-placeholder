d08e3442bfca65b2c64131c8c0e4acb142fcfce4
Chad von Nau <chad@vonnau.com>
2014-04-29 14:39:43 -0700

    Incrementing version number and updating dist files

--- dist/angular-shims-placeholder.js
+++ dist/angular-shims-placeholder.js
@@ -1,7 +1,7 @@
-/*! angular-shims-placeholder - v0.1.0 - 2014-03-21
+/*! angular-shims-placeholder - v0.2.0 - 2014-04-29
 * https://github.com/jrief/angular-shims-placeholder
 * Copyright (c) 2014 Jacob Rief; Licensed MIT */
-(function (angular, undefined) {
+(function (angular, document, undefined) {
   'use strict';
   angular.module('ng.shims.placeholder', []).directive('placeholder', function () {
     if (!angular.mock) {
@@ -14,28 +14,25 @@
       require: 'ngModel',
       priority: 1,
       link: function (scope, elem, attrs, ngModel) {
-        var orig_type = attrs.type;
-        if (!elem.val()) {
-          elem.addClass('empty');
-          elem.val(attrs.placeholder);
-          if (orig_type === 'password') {
-            elem.attr('type', 'text');
-          }
-          ngModel.$setViewValue('');
-        } else {
-          ngModel.$setViewValue(elem.val());
+        var orig_type = attrs.type, is_pwd = orig_type === 'password', text = attrs.placeholder, emptyClassName = 'empty', clone;
+        if (!text) {
+          return;
         }
+        if (is_pwd) {
+          setupPasswordPlaceholder();
+        }
+        setValue(elem.val());
+        ngModel.$setViewValue(elem.val());
         elem.bind('focus', function () {
-          if (elem.hasClass('empty')) {
+          if (elem.hasClass(emptyClassName)) {
             elem.val('');
-            elem.removeClass('empty error');
-            elem.attr('type', orig_type);
+            elem.removeClass(emptyClassName + ' error');
           }
         });
         elem.bind('blur', function () {
           var orig_val = elem.val();
-          setValue(orig_val);
           scope.$apply(function () {
+            setValue(orig_val);
             ngModel.$setViewValue(orig_val);
           });
         });
@@ -44,17 +41,46 @@
         };
         function setValue(val) {
           if (!val) {
-            elem.addClass('empty');
-            elem.val(attrs.placeholder);
-            if (orig_type === 'password') {
-              elem.attr('type', 'text');
+            elem.addClass(emptyClassName);
+            elem.val(text);
+            if (is_pwd) {
+              elem.hide();
+              clone.show();
+              clone.addClass(emptyClassName);
             }
           } else {
-            elem.removeClass('empty');
+            elem.removeClass(emptyClassName);
             elem.val(val);
           }
+        }
+        function setupPasswordPlaceholder() {
+          clone = angular.element('<input/>').attr(angular.extend(extractAttributes(elem[0]), {
+            'type': 'text',
+            'value': text,
+            'placeholder': '',
+            'id': '',
+            'name': ''
+          })).hide().bind('focus', function (e) {
+            clone.hide();
+            elem.show().focus();
+          }).insertBefore(elem);
+          elem.bind('blur', function (e) {
+            if (!elem.val()) {
+              elem.hide();
+              clone.show();
+            }
+          });
+        }
+        function extractAttributes(element) {
+          var attr = element.attributes, copy = {}, skip = /^jQuery\d+/;
+          for (var i = 0; i < attr.length; i++) {
+            if (attr[i].specified && !skip.test(attr[i].name)) {
+              copy[attr[i].name] = attr[i].value;
+            }
+          }
+          return copy;
         }
       }
     };
   });
-}(window.angular));
+}(window.angular, window.document));
