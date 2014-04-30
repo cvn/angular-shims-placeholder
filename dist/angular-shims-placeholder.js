8ba6ba8fcc096448a727db24b8a28bd9db39e453
Chad von Nau <chad@vonnau.com>
2014-04-30 11:06:27 -0700

    Updating dist files

--- dist/angular-shims-placeholder.js
+++ dist/angular-shims-placeholder.js
@@ -1,4 +1,4 @@
-/*! angular-shims-placeholder - v0.2.0 - 2014-04-29
+/*! angular-shims-placeholder - v0.2.0 - 2014-04-30
 * https://github.com/jrief/angular-shims-placeholder
 * Copyright (c) 2014 Jacob Rief; Licensed MIT */
 (function (angular, document, undefined) {
@@ -14,26 +14,27 @@
       require: 'ngModel',
       priority: 1,
       link: function (scope, elem, attrs, ngModel) {
-        var orig_type = attrs.type, is_pwd = orig_type === 'password', text = attrs.placeholder, emptyClassName = 'empty', clone;
+        var orig_val = elem.val() || '', is_pwd = attrs.type === 'password', text = attrs.placeholder, emptyClassName = 'empty', domElem = elem[0], clone;
         if (!text) {
           return;
         }
         if (is_pwd) {
           setupPasswordPlaceholder();
         }
-        setValue(elem.val());
-        ngModel.$setViewValue(elem.val());
+        setValue(orig_val);
+        ngModel.$setViewValue(orig_val);
         elem.bind('focus', function () {
           if (elem.hasClass(emptyClassName)) {
             elem.val('');
-            elem.removeClass(emptyClassName + ' error');
+            elem.removeClass(emptyClassName);
+            elem.removeClass('error');
           }
         });
         elem.bind('blur', function () {
-          var orig_val = elem.val();
+          var val = elem.val();
           scope.$apply(function () {
-            setValue(orig_val);
-            ngModel.$setViewValue(orig_val);
+            setValue(val);
+            ngModel.$setViewValue(val);
           });
         });
         ngModel.$render = function () {
@@ -42,11 +43,10 @@
         function setValue(val) {
           if (!val) {
             elem.addClass(emptyClassName);
-            elem.val(text);
             if (is_pwd) {
-              elem.hide();
-              clone.show();
-              clone.addClass(emptyClassName);
+              showPasswordPlaceholder();
+            } else {
+              elem.val(text);
             }
           } else {
             elem.removeClass(emptyClassName);
@@ -54,22 +54,23 @@
           }
         }
         function setupPasswordPlaceholder() {
-          clone = angular.element('<input/>').attr(angular.extend(extractAttributes(elem[0]), {
+          clone = angular.element('<input/>').attr(angular.extend(extractAttributes(domElem), {
             'type': 'text',
             'value': text,
             'placeholder': '',
             'id': '',
             'name': ''
-          })).hide().bind('focus', function (e) {
-            clone.hide();
-            elem.show().focus();
-          }).insertBefore(elem);
-          elem.bind('blur', function (e) {
-            if (!elem.val()) {
-              elem.hide();
-              clone.show();
-            }
-          });
+          })).addClass(emptyClassName).addClass('ng-hide').bind('focus', hidePasswordPlaceholder);
+          domElem.parentNode.insertBefore(clone[0], domElem);
+        }
+        function showPasswordPlaceholder() {
+          elem.addClass('ng-hide');
+          clone.removeClass('ng-hide');
+        }
+        function hidePasswordPlaceholder() {
+          clone.addClass('ng-hide');
+          elem.removeClass('ng-hide');
+          domElem.focus();
         }
         function extractAttributes(element) {
           var attr = element.attributes, copy = {}, skip = /^jQuery\d+/;
