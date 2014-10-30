4b588d615f6838e1e555a1cc8b6477cfc19f7e21
Chad von Nau <chad@vonnau.com>
2014-10-30 04:24:52 -0700

    Rename demo.html to index.html in preparation for gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
 			'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
 			'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
 			' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-		demo: 'demo.html',
+		demo: 'index.html',
 		// Task configuration.
 		bump: {
 			options: {
